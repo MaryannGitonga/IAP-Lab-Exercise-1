42be5c33d03a7527ab9f4443ba671bba889b9c0e
Maryann Gitonga <34104277+MaryannGitonga@users.noreply.github.com>
2020-10-10 19:18:19 +0300

    Add user CRUD operations

---
+++ operations.php
@@ -0,0 +1,48 @@
+<?php
+
+    class Operations {
+        function __construct(){}
+
+        public function saveUser($pdo, $userName, $userEmail, $userCity, $userPass, $userPhoto){
+            try {
+                $stmt = $pdo->prepare("INSERT INTO users (user_full_name, user_email, city_of_residence, profile_photo, user_password) VALUES (?, ?, ?, ?, ?)");
+                $stmt->execute([$userName, $userEmail, $userCity, $userPhoto, $userPass]);
+                $stmt = null;
+                return "User has been saved";
+            } catch (PDOException $e) {
+                return $e->getMessage();
+            }
+        }
+
+        public function readUser($pdo){
+            try {
+                $stmt=$pdo->prepare("SELECT * FROM users");
+                $stmt->execute();
+                $result = $stmt->fetchAll();
+                $stmt = null;
+                return json_encode($result);
+            } catch (PDOException $e) {
+                return $e->getMessage();
+            }
+        }
+
+        public function searchUser($pdo, $userId)
+        {
+            try {
+                $stmt = $pdo->prepare("SELECT * FROM users WHERE user_id = ?");
+                $stmt->execute([$userId]);
+                $result = $stmt->fetch();
+                $stmt = null;
+                return json_encode($result);
+            } catch (PDOException $e) {
+                return $e->getMessage();
+            }
+        }
+        
+        public function deleteUser()
+        {
+            
+        }
+    }
+
+?>
