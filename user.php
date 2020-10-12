8a90f8c6da3ccdb6fcb0b1ab271b4ead8cc79baf
Maryann Gitonga <34104277+MaryannGitonga@users.noreply.github.com>
2020-10-12 06:17:22 +0300

    Add setters, getters and alterations to register and login methods

--- user.php
+++ user.php
@@ -4,35 +4,111 @@
     session_start();
 
     class User implements Account{
-        private $userName, $userEmail, $userCity, $userPass, $userPhoto;
+        private $userName, $userEmail, $userCity, $userPass, $userPhoto, $inputPass, $newPass;
 
-        public function register($pdo, $userName, $userEmail, $userCity, $userPass, $userPhoto){
+        public function __construct(){
+            $this->userName = "";
+            $this->userEmail = "";
+            $this->userPass = "";
+            $this->userCity = "";
+            $this->userCity = "";
+        }
+
+        public function setUserName($userName)
+        {
+            return $this->userName = $userName;
+        }
+
+        public function setUserEmail($userEmail)
+        {
+            return $this->userEmail = $userEmail;
+        }
+
+        public function setUserPass($userPass){
+            return $this->userPass = $userPass;
+        }
+
+        public function setUserCity($userCity){
+            return $this->userCity = $userCity;
+        }
+
+        public function setUserPhoto($userPhoto){
+            return $this->userPhoto = $userPhoto;
+        }
+
+        public function setInputPass($inputPass){
+            return $this->inputPass = $inputPass;
+        }
+
+        public function setNewPass($newPass){
+            return $this->newPass = $newPass;
+        }
+
+        public function getUserName()
+        {
+            return $this->userName;
+        }
+
+        public function getUserEmail()
+        {
+            return $this->userEmail;
+        }
+
+        public function getUserPass(){
+            return $this->userPass;
+        }
+
+        public function getUserCity(){
+            return $this->userCity;
+        }
+
+        public function getUserPhoto(){
+            return $this->userPhoto;
+        }
+
+        public function getInputPass(){
+            return $this->inputPass;
+        }
+
+        public function getNewPass(){
+            return $this->newPass;
+        }
+
+        public function register($pdo){
             try {
                 $stmt = $pdo->prepare("INSERT INTO users (user_full_name, user_email, city_of_residence, profile_photo, user_password) VALUES (?, ?, ?, ?, ?)");
-                $stmt->execute([$userName, $userEmail, $userCity, $userPhoto, $userPass]);
+                $stmt->execute([$this->userName, $this->userEmail, $this->userCity, $this->userPhoto, $this->userPass]);
                 $stmt = null;
                 return "User has been saved";
             } catch (PDOException $e) {
+                echo "here";
                 return $e->getMessage();
             }
         }
-        public function login($pdo, $userEmail, $userPass){
+        public function login($pdo){
             try {
-                $stmt = $pdo->prepare("SELECT * FROM users WHERE user_full_name = ? AND user_password = ?");
-                $stmt->execute([$userName, $userPass]);
+                $stmt = $pdo->prepare("SELECT user_password FROM users WHERE user_email = ?");
+                $stmt->execute([$this->userEmail]);
                 $result = $stmt->fetch();
-                print_r($result);
-                $stmt = null;
-                return json_encode($result);
+                $this->userPass = $result['user_password'];
+                if (password_verify($this->inputPass, $this->userPass)) {
+                    $stmt = $pdo->prepare("SELECT * FROM users WHERE user_email = ? AND user_password = ?");
+                    $stmt->execute([$this->userEmail, $this->userPass]);
+                    $result = $stmt->fetch();
+                    $stmt = null;
+                return $result;
+                } else {
+                    echo 'Invalid password.';
+                }
             } catch (PDOException $e) {
                 return $e->getMessage();
             }
         }
-        public function changePassword($pdo, $userPass)
+        public function changePassword($pdo)
         {
             try {
                 $stmt = $pdo->prepare("UPDATE users SET user_password = $userPass WHERE user_id = ?");
-                $stmt->execute([$userPass, $_SESSION['id']]);
+                $stmt->execute([$this->userPass, $_SESSION['id']]);
                 $result = $stmt->fetch();
                 $stmt = null;
                 return "User Password has been changed";
@@ -42,6 +118,10 @@
         }
         public function logout($pdo){
             session_destroy();
+        }
+
+        public function __destruct(){
+
         }
     }
 
