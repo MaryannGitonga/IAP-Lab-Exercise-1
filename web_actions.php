ab56a8926661171f00e70f71977bd1b67c99f813
Maryann Gitonga <34104277+MaryannGitonga@users.noreply.github.com>
2020-10-12 07:25:07 +0300

    Add login logic

--- web_actions.php
+++ web_actions.php
@@ -1,7 +1,6 @@
 <?php
 
     include_once 'db_connect.php';
-    include_once 'operations.php';
     include_once 'user.php';
     session_start();
 
@@ -13,7 +12,7 @@
         $userName = $_POST["full-name"];
         $userEmail = $_POST["email"];
         $userCity = $_POST["residence"];
-        $userPhoto = $_FILES["profile-photo"]['name'];
+        $userPhoto = $_FILES["profile-photo"];
         $userPass = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
         //user object
@@ -27,5 +26,23 @@
         $message = $user->register($pdo);
         echo $message;
         header("Location: /Simple-UI/templates/login.php");
+    }
+    
+    if (isset($_POST['login'])) {
+        $userEmail = $_POST["email"];
+        $userPass = $_POST["password"];
+
+        $user = new User();
+        $user->setUserEmail($userEmail);
+        $user->setInputPass($userPass);
+        $user_details = $user->login($pdo);
+        
+        $_SESSION['user_id'] = $user_details['user_id'];
+        $_SESSION['user_name'] = $user_details['user_full_name'];
+        $_SESSION['user_email'] = $user_details['user_email'];
+        $_SESSION['city'] = $user_details['city_of_residence'];
+        $_SESSION['photo'] = $user_details['profile_photo'];
+
+        header("Location: /Simple-UI/templates/index.php");
     }
 ?>
