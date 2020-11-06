83e0abe76dc0ce4024a5653a3b22b2c9276f5f66
Maryann Gitonga <34104277+MaryannGitonga@users.noreply.github.com>
2020-11-06 07:51:42 +0300

    Add html for the products page

--- templates/index.php
+++ templates/index.php
@@ -10,11 +10,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../font-awesome/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
-    <div class="container" id="container">
-        <div class="profile-container">
+    <div class="container text-center" id="container">
+        <!-- <div class="profile-container">
             <img src="<?php echo "images/".$_SESSION['photo'] ?>" alt="profile-pic">
         </div>
         <div class="overlay-container">
@@ -27,6 +28,39 @@
                     <p>City of Residence: <?php echo $_SESSION['city'] ?></p>
                     <button class="sm-btn"><a href="change-password.php">Change Password</a></button>
                     <button class="sm-btn"><a href="../logout.php">Logout</a></button>
+                    </div>
+                </div>
+            </div>
+        </div> -->
+
+        <div class="shell">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="food-product">
+                            <div class="food-img">
+                            <!--food image goes in here-->
+                                <img src="" alt="">
+                            </div>
+                            <div class="food-text">
+                                <div class="food-title">
+                                    <h3><!--food title goes in here--></h3>
+                                </div>
+                                <div class="food-description">
+                                    <p><!--food description goes here--></p>
+                                </div>
+                                <div class="food-price">
+                                    <div class="price-left">
+                                        <span class="price"><!--food price goes in here--></span>
+                                    </div>
+                                    <div class="price-right">
+                                        <a href="#" class="cart-btn">
+                                            <i class="fa fa-shopping-bag" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
