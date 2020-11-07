bbba10e9af54e3707151597c12fc97c45524aa2a
Maryann Gitonga <34104277+MaryannGitonga@users.noreply.github.com>
2020-11-07 10:38:51 +0300

    Add feature coming soon modal

--- templates/index.php
+++ templates/index.php
@@ -12,13 +12,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="css/styles.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></head>
 <body>
     <div class="container text-center">
         <h1>Order Food Items</h1>
         <div class="shell">
-            <div class="">
+            <div class="container">
                 <div class="row">
                     <?php
                         include_once '../db_connect.php';
@@ -47,9 +46,24 @@
                                                 <span class="price">Ksh <?php echo $food['food_price'] ?></span>
                                             </div>
                                             <div class="price-right">
-                                                <a href="../web_actions.php?add-to-cart=<?php echo $food['food_id'] ?>" class="cart-btn">
+                                                <a href="" class="cart-btn" data-toggle="modal" data-target="#comingsoonModal">
                                                     <i class="fa fa-shopping-bag" aria-hidden="true"></i>
                                                 </a>
+                                            </div>
+                                            <div class="modal fade" id="comingsoonModal" tabindex="-1" role="dialog" aria-labelledby="comingsoonModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog" role="document">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title" id="comingsoonModalLabel">This Feature Is Coming Soon</h5>
+                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                            <span aria-hidden="true">&times;</span>
+                                                            </button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            This feature will be available to you soon!
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
@@ -62,5 +76,11 @@
             </div>
         </div>
     </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
+
+<!-- For the cart btn -->
+<!-- "../web_actions.php?add-to-cart=<?php //echo $food['food_id'] ?>" -->
